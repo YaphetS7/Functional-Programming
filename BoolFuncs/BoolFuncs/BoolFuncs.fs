649b099dc42f5d2d4d6dfa47ca2a1f70ae6848bd
YaphetS7 <interni17121999@mail.ru>
2019-07-06 00:29:44 +0400

    SomeRecursion

---
+++ BoolFuncs/BoolFuncs/BoolFuncs.fs
@@ -0,0 +1,14 @@
+﻿// 16.1
+let notDivisible (n, m) = m % n = 0
+
+// 16.2
+let rec loop_func = function 
+    | (n, i) when n = i -> true
+    | (n, i) when (n % i) = 0 -> false
+    | (n, i) -> loop_func(n, i+ 1)
+
+let prime = function
+    | 1 -> false
+    | 2 -> true
+    | n -> loop_func(n, 2)
+
