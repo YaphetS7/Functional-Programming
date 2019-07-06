e317f84da6037db9a78cd7211cfe549aabf2b969
YaphetS7 <interni17121999@mail.ru>
2019-07-06 16:37:54 +0400

    Currying

---
+++ CurryingAndComposition/CurryingAndComposition/Program.fs
@@ -0,0 +1,12 @@
+﻿// 20.3.1
+let vat n x = x*(1.0 + float(n / 100))
+
+// 20.3.2
+let unvat n x = x * 100.0 / float(n + 100)
+
+// 20.3.3
+let rec find_min = function
+    |(f,n) when f(n) = 0 -> n
+    |(f,n) -> find_min(f,n+1)
+
+let rec min f = find_min(f, 1)
