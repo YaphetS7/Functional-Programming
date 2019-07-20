4ff47d06550ffdc3376021bfaaaa735bf6cf6c00
YaphetS7 <interni17121999@mail.ru>
2019-07-20 21:55:47 +0400

    List

---
+++ List/List/Program.fs
@@ -0,0 +1,16 @@
+﻿// 34.1
+let rec upto = function
+    |n -> 1 :: [2..n]
+    
+// 34.2
+let rec dnto = function
+    |1 -> [1] 
+    |n -> n :: dnto (n-1)
+    
+//34.3
+let rec iter = function
+    |(n,i) when i <= n -> i :: iter(n , i + 2)
+    |(n,i) when i > n -> []
+    |(n,i) -> []
+let rec evenn = function
+    |n -> iter(n, 0)
