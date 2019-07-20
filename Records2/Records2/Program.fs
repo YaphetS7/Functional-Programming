2b1795015769da0d48fdf8c1404aa339aff96b3b
YaphetS7 <interni17121999@mail.ru>
2019-07-20 20:37:22 +0400

    CompareTimeOfDays

---
+++ Records2/Records2/Program.fs
@@ -0,0 +1,14 @@
+﻿type F = 
+  | AM
+  | PM
+
+type TimeOfDay = { hours : int; minutes : int; f: F }
+
+let (.>.) x y =
+    if x.f = y.f then
+        if x.hours = y.hours then
+            x.minutes > y.minutes
+        else
+            x.hours > y.hours
+    else
+        x.f > y.f
